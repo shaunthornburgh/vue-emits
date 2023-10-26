0ebc74d884498cbabda191716b284ab27c7b9437
Shaun Thornburgh <personal@shaunthornburgh.com>
2023-10-26 22:06:08 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <h1 class="text-base font-semibold leading-7 text-gray-900 pt-8">Parent Component</h1>
+    <p>Button has been clicked: {{ count }} times</p>
+    <ChildComponent @clicked="increaseCount" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import ChildComponent from './components/ChildComponent.vue'
+
+const count = ref(0)
+
+function increaseCount() {
+  count.value++
+}
+</script>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <button
+      @click="notifyParent"
+      type="button"
+      class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-6"
+  >Click me!</button>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue'
+
+const emit = defineEmits(['clicked'])
+
+function notifyParent() {
+  emit('clicked')
+}
+</script>
